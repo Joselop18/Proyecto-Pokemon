2da95cdda87f823b597a2dca27cf1df5cb2c8168
Joselop18 <aaguilar-2023496@kinal.edu.gt>
2024-08-30 23:43:24 -0600

    Buscadores de Pokemones

--- src/components/BuscarPokemon.jsx
+++ src/components/BuscarPokemon.jsx
@@ -7,7 +7,7 @@ export const BuscarPokemon = ({handleGetPokemonInfo}) => {
       <div className="container d-flex flex-row justify-content-center alig-items-center mt-3 w-50">
         <form className="d-flex" action="" onSubmit={(e)=>{handleGetPokemonInfo(pokemon,e)}}>
           <input type="text" className="form-control me-2" placeholder="Search" onChange={(e)=>{setPokemon(e.target.value)}} />
-          <input type="submit" value="Buscar gif" className="btn btn-outline-success-white btn btn-dark" />
+          <input type="submit" value="Buscar Pokemon" className="btn btn-outline-danger" />
         </form>
       </div>
     </>
